8ca432cc49ff1fe938b958da7f7464e85451a7bd
Nick Rogers and William Kinzig <nick@>
2018-06-12 16:40:13 -0700

    Add fully functioning movie ticket calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -9,42 +9,65 @@ function Movie (name, times) {
   this.listedTimes = times;
 
 }
-Ticket.prototype.priceCalc = function() {
-  var price = 10;
-  if (this.age === "child") {
+Ticket.prototype.priceCalc = function(index) {
+  price = 10;
+  if (this.age[index] === "child") {
     price *= 0.6;
-  } else if (this.age === "senior"){
+  } else if (this.age[index] === "senior"){
     price *= 0.8;
   } else {
     price *= 1.2;
   }
-  if (this.time < 1400) {
+  if (parseInt(this.time) < 1400) {
     price *= 0.8;
   }
+  return price;
 }
 
-function pushNumberOfTickets (array, index, numAdult, numChild, numSenior) {
+function pushNumberOfTickets (array, numAdult, numChild, numSenior) {
   for(i = 0; i < numAdult; i++){
     array.push("adult");
-    index++;
   }
   for(i = 0; i < numChild; i++){
     array.push("child");
-    index++;
   }
   for(i = 0; i < numSenior; i++){
     array.push("senior");
-    index++;
   }
 }
 
 
 function toMilitaryTime (string) {
   var newString = string.split(":").join("");
-  if(newString.includes("am")){
-    return parseInt(newString);
+  if(newString.includes("pm")) {
+    if(parseInt(newString) < 1200) {
+      return parseInt(newString)+ 1200;
+    } else {
+      return parseInt(newString);
+    }
   } else {
-    return parseInt(newString) + 1200;
+    return parseInt(newString);
+  }
+}
+function toNormalTime (string) {
+  var number = parseInt(string);
+  if (number >= 1300) {
+    number -= 1200
+    if (number < 1000) {
+      newString = number.toString();
+      return newString.slice(0,1) + ":" + newString.slice(1) + "pm";
+    } else {
+      newString = number.toString();
+      return newString.slice(0,2) + ":" + newString.slice(2) + "pm";
+    }
+  } else {
+    if (number < 1000) {
+      newString = number.toString();
+      return newString.slice(0,1) + ":" + newString.slice(1) + "am";
+    } else {
+      newString = number.toString();
+      return newString.slice(0,2) + ":" + newString.slice(2) + "am";
+    }
   }
 }
 var adriftTimes = ["11:10am", "1:45pm", "4:20pm", "7:10pm", "9:45pm"];
@@ -86,10 +109,19 @@ $(document).ready(function(){
     var inputtedNumSeniors = parseInt($("input#senior").val());
 
     var arrayOfAges = [];
-    var agesIndex = 0;
-    pushNumberOfTickets(arrayOfAges, agesIndex, inputtedNumAdults, inputtedNumChilds, inputtedNumSeniors);
-    var newTicket = new Ticket (movieNames[index], inputtedTime, arrayOfAges[agesIndex]);
+    pushNumberOfTickets(arrayOfAges, inputtedNumAdults, inputtedNumChilds, inputtedNumSeniors);
 
-    
+    var newTicket = new Ticket (movieNames[index], inputtedTime, arrayOfAges);
+
+    var total = 0;
+
+    for(i = 0; i < arrayOfAges.length; i++) {
+      total += newTicket.priceCalc(i);
+    };
+
+    $("#movie-summary").text(newTicket.movieName);
+    $("#time-summary").text(toNormalTime(inputtedTime));
+    $("#total").text(total.toFixed(2));
+    $(".summary-hidden").show();
   });
 });
