4cd9992d43e7aa3ab77044bd992ed5890d4802cc
Nick Rogers and William Kinzig <nick@>
2018-06-12 13:38:46 -0700

    Add row formatting.

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ function Ticket (movie, time, age) {
 function Movie (name, times) {
   this.name = name;
   this.listedTimes = times;
+
 }
 
 var adriftTimes = ["11:10am", "1:45pm", "4:20pm", "7:10pm", "9:45pm"];
@@ -15,27 +16,30 @@ var bookClubTimes = ["12:45pm", "3:35pm", "6:20pm", "9:00pm"]
 var firstReformedTimes = ["12:10pm", "3:00pm", "7:15pm", "9:35pm"]
 var rBGTimes = ["11:30am", "2:00pm", "4:30pm", "7:05pm"]
 var movieTimes = [adriftTimes, avengersTimes, bookClubTimes, firstReformedTimes, rBGTimes];
+var movieNames = ["Adrift", "Avengers:Infinity War","Book Club", "First Reformed", "RBG"]
 
 $(document).ready(function(){
   $("button.show-time-button").click(function(){
     $("#show-times").empty();
-    var inputtedMovie = parseInt($("select#movie-name").val());
-    var i = inputtedMovie;
-    var newMovie = new Movie (inputtedMovie, movieTimes[i]);
+    var index = parseInt($("select#movie-name").val());
+    var newMovie = new Movie (movieNames[index], movieTimes[index]);
+    if (!isNaN(index)) {
+      $(".movie-title").text("Show Times for " + newMovie.name);
 
-    newMovie.listedTimes.forEach(function(time){
+
+      newMovie.listedTimes.forEach(function(time){
       $("#show-times").append('<div class="radio">' +
-                                '<label>' +
-                                  '<input type="radio" name="flavor" value="' + time + '" checked>' +
-                                  time +
-                                '</label>' +
-                              '</div>');
-    });
+                                  '<label>' +
+                                    '<input type="radio" name="flavor" value="' + time + '" checked>' +
+                                    time +
+                                  '</label>' +
+                                '</div>');
+      });
+    } else {
+      $(".movie-title").empty();
+    }
   });
   $("form#buy-tickets").submit(function(event){
     event.preventDefault();
-
-
-
   });
 });
